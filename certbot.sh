517393cea9ab8d7369c1b6145f05d27bef11eb63
Bryce Fisher-Fleig <hckr1292@gmail.com>
2018-01-20 13:30:06 -0800

    (fix) Typo in default REGION

--- certbot.sh
+++ certbot.sh
@@ -8,7 +8,7 @@ certbot --agree-tos \
   -m "$EMAIL" \
   -a certbot-s3front:auth \
   --certbot-s3front:auth-s3-bucket "$S3_BUCKET" \
-  --certbot-s3front:auth-s3-region "${REGION:-'us-east-1'}" \
+  --certbot-s3front:auth-s3-region "${REGION:-us-east-1}" \
   --certbot-s3front:auth-s3-directory "${DIRECTORY:-}" \
   -i certbot-s3front:installer \
   --certbot-s3front:installer-cf-distribution-id "$CLOUDFRONT_DISTRIBUTION_ID" \
